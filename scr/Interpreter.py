ac99826acf62cdc43b2c709ee3b663de06375139
4xvoid <94848470+4xvoid@users.noreply.github.com>
2023-06-23 21:51:48 -0400

    Update Interpreter.py

--- scr/Interpreter.py
+++ scr/Interpreter.py
@@ -10,19 +10,23 @@ def Error(message, exitCode):
 
 def Lex(source):
     print("Lexing Source.")
+
     tokens = []
     token = ""
     isString = False
-
     i = 0
+
     while i < len(source): 
         if source[i] == " " or source[i] == "\n":
             if not isString:
                 tokens.append(token)
                 token = ""
-        elif source[i] == "#":
+
+        elif source[i] == "#" and not isString:
             while source[i] != "\n":
                 i += 1
+                token = ""
+
         elif source[i].isupper():
             c = source[i]
 
@@ -38,6 +42,7 @@ def Lex(source):
                     Error(f"Subscript Not Found On Variable '{c}'.", 1)
 
             token += "}"
+            
         else:
             token += source[i]
 
@@ -52,46 +57,40 @@ def Lex(source):
 
     return tokens
 
-def Parse(tokens):
-    print("Parsing Tokens.")
-    i = 0
-
-    while i < len(tokens):
-        if tokens[i] == "define":
-            pass
-        elif tokens[i] == "include":
-            pass
-        elif tokens[i].startswith('"') and tokens[i].endswith('"'):
-            pass
-        elif tokens[i] != "":
-            Error(f"Unknown Token '{tokens[i]}'.", 1)
-
-        i += 1
-
 def Compile(tokens):
     api = Desmos.API()
     defined = {}
 
     i = 0
     id = 1
+    color = "#000000"
 
     while i < len(tokens):
         if tokens[i] == "define":
             defined[tokens[i + 1]] = tokens[i + 2]
             i += 2
+
         elif tokens[i] == "include":
-            fileName = tokens[i + 1][1:-1]
+            fileName = tokens[i+1][1:-1]
             del tokens[i:i+2]
             Compile(Lex(Read(fileName)) + tokens)
             return
+        
+        elif tokens[i] == "color":
+            i += 1
+            color = tokens[i][1:-1]
+
         elif tokens[i].startswith('"') and tokens[i].endswith('"'):
             latex = tokens[i][1:-1]
 
             for define in defined:
                 latex = latex.replace(define, defined[define][1:-1])
 
-            api.SetExpression(str(id), latex.replace(" ", ""))
+            api.SetExpression(str(id), latex.replace(" ", ""), color)
             id += 1
+            
+        elif tokens[i] != "":
+            Error(f"Unknown Token '{tokens[i]}'.", 1)
 
         i += 1
 
